1780398d058cfca556b6efaf18fb7a01b2ba7034
azharjk <azhr.project@gmail.com>
2023-02-16 06:55:39 +0700

    move UpdateProfileController to Profile directory

--- routes/profile.php
+++ routes/profile.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\UpdateProfileController;
+use App\Http\Controllers\Profile\UpdateProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::put('/update-profile', UpdateProfileController::class)
+Route::put('/profile/update', UpdateProfileController::class)
     ->middleware('auth')
-    ->name('updateProfile');
+    ->name('profile.update');
