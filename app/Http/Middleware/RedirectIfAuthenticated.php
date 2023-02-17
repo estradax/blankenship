8a9c20d27c74d56553b3cb6d7343f8ff612403fb
azharjk <azhr.project@gmail.com>
2023-02-17 17:07:51 +0700

    Revert "fix return type"
    This reverts commit c22ae72ffe5b7ef2b69820db3a57fadd9d083bd4.

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use App\Providers\RouteServiceProvider;
 use Closure;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -15,7 +14,7 @@ class RedirectIfAuthenticated
     /**
      * Handle an incoming request.
      */
-    public function handle(Request $request, Closure $next, mixed ...$guards): Response|JsonResponse|RedirectResponse
+    public function handle(Request $request, Closure $next, mixed ...$guards): Response|RedirectResponse
     {
         $guards = empty($guards) ? [null] : $guards;
 
